cd33c743b3bd9cfea14754f5d41496afb46811c6
= <bibekaryal0@gmail.com>
2020-08-11 10:39:27 +0545

    index page

--- pages/index.vue
+++ pages/index.vue
@@ -21,6 +21,7 @@
 <!--      </div>-->
 <!--    </div>-->
 <!--  </div>-->
+
   <div class="container-fluid">
     <div class="row d-flex justify-content-between align-items-center">
       <div class="col-lg-3 rd-logo">
@@ -106,44 +107,124 @@
     </div>
   </div>
 <hr>
-    <div class="container-fluid">
+
+    <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <h1>News</h1>
+          <h1> प्रदेश</h1>
+          <hr>
         </div>
         <div class="col-md-9">
-          <div class="md-3">
-        <div class="row">
-          <div class="col-md-4">
-            <div class="cat-post">
-              <a href="https://www.jhilko.com/news/5784/">
-                <img src="https://www.jhilko.com/wp-content/uploads/2020/08/hadtal-400x225.jpg" alt="खानामा किरा भेटिएपछि आईसोलेसनका संक्रमित भोक हडतालमा" height="100px">
-              </a>
-              <div class="cat-info">
-                <a href="https://www.jhilko.com/news/5784/"><h3>खानामा किरा भेटिएपछि आईसोलेसनका संक्रमित भोक हडतालमा</h3></a>
+          <div class="mt-3">
+            <div class="row">
+              <div class="col-md-4">
+                <div class="cat-post">
+                  <a href="https://www.jhilko.com/province/province-2/6042/">
+                    <img src="https://www.jhilko.com/wp-content/uploads/2020/08/aandolan-press-400x225.jpg" alt="उपचार नपाई मृत्यु भएपछि वीरगञ्जमा चिकित्सकको गाडी तोडफोड" class="img-fluid lazy">
+                  </a>
+                  <hr>
+                  <div class="cat-info">
+                    <a href="https://www.jhilko.com/province/province-2/6042/"><h3>उपचार नपाई मृत्यु भएपछि वीरगञ्जमा चिकित्सकको गाडी तोडफोड</h3></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="cat-post">
+                  <a href="https://www.jhilko.com/province/province-5/6034/">
+                    <img src="https://www.jhilko.com/wp-content/uploads/2020/08/nepalgunj-seal-400x225.png" alt="नेपालगञ्ज सेनाको निगरानीमा सिल" class="img-fluid lazy">
+                  </a>
+                  <hr>
+                  <div class="cat-info">
+                    <a href="https://www.jhilko.com/province/province-5/6034/"><h3>नेपालगञ्ज सेनाको निगरानीमा सिल</h3></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="cat-post">
+                  <a href="https://www.jhilko.com/province/6023/">
+                    <img src="https://www.jhilko.com/wp-content/uploads/2020/06/dead-1-400x225.jpg" alt="वीरगञ्जमा उपचार नपाएर ज्यान गुमाएकी महिलामा कोरोना सङ्क्रमण पुष्टि" class="img-fluid lazy">
+                  </a>
+                  <hr>
+                  <div class="cat-info">
+                    <a href="https://www.jhilko.com/province/6023/"><h3>वीरगञ्जमा उपचार नपाएर ज्यान गुमाएकी महिलामा कोरोना सङ्क्रमण पुष्टि</h3></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="cat-post">
+                  <a href="https://www.jhilko.com/news/6019/">
+                    <img src="https://www.jhilko.com/wp-content/uploads/2020/06/death-2-400x225.jpg" alt="भक्तपुरको जगातीमा मोटरसाइकल दुर्घटना, एकको मृत्यु" class="img-fluid lazy">
+                  </a>
+                  <hr>
+                  <div class="cat-info">
+                    <a href="https://www.jhilko.com/news/6019/"><h3>भक्तपुरको जगातीमा मोटरसाइकल दुर्घटना, एकको मृत्यु</h3></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="cat-post">
+                  <a href="https://www.jhilko.com/province/province-2/6017/">
+                    <img src="https://www.jhilko.com/wp-content/uploads/2020/07/corona-2-400x225.jpg" alt="भारतीय महावाणिज्य दुतावासका कर्मचारीसहित पर्सामा थप ७१ जनामा कोरोना संक्रमण पुष्टि" class="img-fluid lazy">
+                  </a>
+                  <hr>
+                  <div class="cat-info">
+                    <a href="https://www.jhilko.com/province/province-2/6017/"><h3>भारतीय महावाणिज्य दुतावासका कर्मचारीसहित पर्सामा थप ७१ जनामा कोरोना संक्रमण&hellip;</h3></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="cat-post">
+                  <a href="https://www.jhilko.com/province/province-2/6017/">
+                    <img src="https://www.jhilko.com/wp-content/uploads/2020/07/corona-2-400x225.jpg" alt="भारतीय महावाणिज्य दुतावासका कर्मचारीसहित पर्सामा थप ७१ जनामा कोरोना संक्रमण पुष्टि" class="img-fluid lazy">
+                  </a>
+                  <hr>
+                  <div class="cat-info">
+                    <a href="https://www.jhilko.com/province/province-2/6017/"><h3>भारतीय महावाणिज्य दुतावासका कर्मचारीसहित पर्सामा थप ७१ जनामा कोरोना संक्रमण&hellip;</h3></a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-12">
+                <div aria-label="Page navigation example"><ul class="pagination pagination-lg">
+<!--                  <li class="active page-item" class="page-item"><a href="https://www.jhilko.com/category/province/" class="page-link">1</a></li>-->
+                  <li class="page-item"><a href="https://www.jhilko.com/category/province/page/2/" class="page-link">2</a></li>
+                  <li class="page-item"><a href="https://www.jhilko.com/category/province/page/3/" class="page-link">3</a></li>
+                  <li  class="page-item"><a href="#" class="page-link">...</a></li><li  class="page-item"><a href="https://www.jhilko.com/category/province/page/30/" class="page-link">30</a></li>
+                  <li  class="page-item"><i class="page-link"><a href="https://www.jhilko.com/category/province/page/2/" >Next Page &raquo;</a></i></li>
+                </ul></div>
               </div>
             </div>
           </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="rd-popular p-3">
+            <div class="rd-title text-center">
+              <h2><span>धेरै पढिएका</span></h2>
+            </div>
+            <div class="rd-wrap">
+              <ul>
+                <li><a href="https://www.jhilko.com/health/4942/">बुटवलको लुम्बिनी सीटी अस्पतालमा यसरी हुन्छ बिरामीको खरिद बिक्रि&#8230;</a></li>
+                <li><a href="https://www.jhilko.com/politics/4662/">&#8216;जनार्दन शर्मा पनि दलाल बनाइएछन्&#8217;</a></li>
+                <li><a href="https://www.jhilko.com/news/4055/">कान्तिपुर पब्लिकेशन्सलाई पत्रकारहरुको &#8216;दुई रुपैयाँ&#8217;</a></li>
+                <li><a href="https://www.jhilko.com/exciting/4898/">पारदर्शी पीपीईले भाग्य खुल्यो नर्सको</a></li>
+                <li><a href="https://www.jhilko.com/news/4578/">श्रीमती मार्ने पूर्व डीआईजी रञ्जन कोइरालाको फैसलाविरुद्ध सरकारी वकील पुनरावलोकन जाने</a></li>
+                <li><a href="https://www.jhilko.com/politics/4263/">नेकपामा बिचौलिया निर्णायक</a></li>
+                <li><a href="https://www.jhilko.com/views/4766/">राष्ट्रपतिमा पञ्चायती शैली र गन्ध</a></li>
+                <li><a href="https://www.jhilko.com/health/4158/">लकडाउन खुल्यो, अब के गर्ने ?</a></li>
+              </ul>
+            </div>                </div>
+        </div>
       </div>
-
     </div>
-    </div>
-      </div>
-    </div>
-
-
 
   </div>
-<!--  <div class="row">-->
-<!--    <div class="col-sm-4">-->
-<!--      <img src="https://www.jhilko.com/wp-content/themes/Jhilko/img/jhilko-logo.svg" width="300px"  class="img-fluid lazy">-->
 
-<!--    </div>-->
-<!--    <div class="col-sm-6">-->
-<!--      <img src="https://www.jhilko.com/wp-content/themes/Jhilko/img/jhilko-logo.svg" width="300px" height="50px" class="img-fluid lazy">-->
 
-<!--    </div>-->
-<!--  </div>-->
+
+
+
+
 
 
 
