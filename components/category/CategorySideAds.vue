63d1e427c711db35cfb4caca28248ab2b38ec3a5
codera21 <atul.nepal@gmail.com>
2020-08-30 21:39:15 +0545

    ads component

---
+++ components/category/CategorySideAds.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="cat-side-ads">
+    <div v-for="item in items" :key="item.id" class="advertisement my-2">
+      <figure>
+        <a :href="adLink" :title="title" target="_blank">
+          <img :src="item.src" alt />
+        </a>
+      </figure>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          id: 0,
+          adLink: "#",
+          title: "ads title 0",
+          src: "https://via.placeholder.com/330X90?text=Your Ad Here",
+          isGif: false,
+        },
+        {
+          id: 1,
+          adLink: "#",
+          title: "ads title 1",
+          src: "https://via.placeholder.com/330X90?text=Your Ad Here",
+          isGif: false,
+        },
+      ],
+    };
+  },
+};
+</script>
--- pages/category-page.vue
+++ pages/category-page.vue
@@ -1,138 +1,123 @@
 <template>
-<div class="categorypage-container">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-9">
-            <section class="threeGrid-section">
-              <div class="grid-title"><span>अन्तर्राष्ट्रिय</span></div>
-              <div class="row">
-                <div class="col-md-4 ">
-                  <div class="threeGrid-section-card newsCard">
-                    <nuxt-link to="/single-page">
-                      <img src="http://lorempixel.com/400/226/" alt="" class="img-fluid" />
-                    </nuxt-link>
-                    <h4>
-                      <nuxt-link to="/single-page">
-                        उपत्यकामा एकैदिन भेटिए ३०१ कोरोना संक्रमित
-                      </nuxt-link>
-                    </h4>
-                  </div>
+  <div class="categorypage-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-9">
+          <section class="threeGrid-section">
+            <div class="grid-title">
+              <span>अन्तर्राष्ट्रिय</span>
+            </div>
+            <div class="row">
+              <div class="col-md-4">
+                <div class="threeGrid-section-card newsCard">
+                  <nuxt-link to="/single-page">
+                    <img src="http://lorempixel.com/400/226/" alt class="img-fluid" />
+                  </nuxt-link>
+                  <h4>
+                    <nuxt-link to="/single-page">उपत्यकामा एकैदिन भेटिए ३०१ कोरोना संक्रमित</nuxt-link>
+                  </h4>
                 </div>
-                <div class="col-md-4 ">
-                  <div class="threeGrid-section-card">
-                    <nuxt-link to="/single-page">
-                      <img src="http://lorempixel.com/400/226/" alt="" class="img-fluid" />
-                    </nuxt-link>
-                    <h4>
-                      <nuxt-link to="/single-page">
-                        उपत्यकामा एकैदिन भेटिए ३०१ कोरोना संक्रमित
-                      </nuxt-link>
-                    </h4>
-                  </div>
-                </div>
-                <div class="col-md-4 ">
-                  <div class="threeGrid-section-card">
-                    <nuxt-link to="/single-page">
-                      <img src="http://lorempixel.com/400/226/" alt="" class="img-fluid" />
-                    </nuxt-link>
-                    <h4>
-                      <nuxt-link to="/single-page">
-                        उपत्यकामा एकैदिन भेटिए ३०१ कोरोना संक्रमित
-                      </nuxt-link>
-                    </h4>
-                  </div>
-                </div><div class="col-md-4 ">
-                  <div class="threeGrid-section-card">
-                    <nuxt-link to="/single-page">
-                      <img src="http://lorempixel.com/400/226/" alt="" class="img-fluid" />
-                    </nuxt-link>
-                    <h4>
-                      <nuxt-link to="/single-page">
-                        उपत्यकामा एकैदिन भेटिए ३०१ कोरोना संक्रमित
-                      </nuxt-link>
-                    </h4>
-                  </div>
-                </div>
-
               </div>
-            </section>
-            <nav aria-label="..." class="my-3">
-              <ul class="pagination justify-content-center">
-                <li class="page-item disabled">
-                  <a class="page-link" href="#" tabindex="-1">Previous</a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item active">
-                  <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item">
-                  <a class="page-link" href="#">Next</a>
-                </li>
-              </ul>
+              <div class="col-md-4">
+                <div class="threeGrid-section-card">
+                  <nuxt-link to="/single-page">
+                    <img src="http://lorempixel.com/400/226/" alt class="img-fluid" />
+                  </nuxt-link>
+                  <h4>
+                    <nuxt-link to="/single-page">उपत्यकामा एकैदिन भेटिए ३०१ कोरोना संक्रमित</nuxt-link>
+                  </h4>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="threeGrid-section-card">
+                  <nuxt-link to="/single-page">
+                    <img src="http://lorempixel.com/400/226/" alt class="img-fluid" />
+                  </nuxt-link>
+                  <h4>
+                    <nuxt-link to="/single-page">उपत्यकामा एकैदिन भेटिए ३०१ कोरोना संक्रमित</nuxt-link>
+                  </h4>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="threeGrid-section-card">
+                  <nuxt-link to="/single-page">
+                    <img src="http://lorempixel.com/400/226/" alt class="img-fluid" />
+                  </nuxt-link>
+                  <h4>
+                    <nuxt-link to="/single-page">उपत्यकामा एकैदिन भेटिए ३०१ कोरोना संक्रमित</nuxt-link>
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </section>
+          <nav aria-label="..." class="my-3">
+            <ul class="pagination justify-content-center">
+              <li class="page-item disabled">
+                <a class="page-link" href="#" tabindex="-1">Previous</a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">1</a>
+              </li>
+              <li class="page-item active">
+                <a class="page-link" href="#">
+                  2
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">3</a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="#">Next</a>
+              </li>
+            </ul>
           </nav>
-      </div>
+        </div>
 
-      <div class="col-md-3">
-        <div class="rightsection">
-          <div class="title">
-            <h5>
-              लोकप्रिय
-            </h5>
+        <div class="col-md-3">
+          <div class="rightsection">
+            <div class="title">
+              <h5>लोकप्रिय</h5>
+            </div>
+
+            <ul class>
+              <li>
+                <a class="mini-newscard">
+                  <figure>
+                    <img
+                      src="http://bg.annapurnapost.com/uploads/media/balatkar-case-morang_20200828030807.jpg"
+                      alt
+                    />
+                  </figure>
+                  <h6>किशोरीमाथि तीन बालकलाई बलात्कार गर्न लगाएपछि</h6>
+                </a>
+                <a class="mini-newscard">
+                  <figure>
+                    <img
+                      src="http://bg.annapurnapost.com/uploads/media/balatkar-case-morang_20200828030807.jpg"
+                      alt
+                    />
+                  </figure>
+                  <h6>किशोरीमाथि तीन बालकलाई बलात्कार गर्न लगाएपछि</h6>
+                </a>
+              </li>
+            </ul>
           </div>
-
-          <ul class="">
-            <li>
-              <a class="mini-newscard">
-                <figure>
-                  <img src="http://bg.annapurnapost.com/uploads/media/balatkar-case-morang_20200828030807.jpg" alt="">
-                </figure>
-                <h6>किशोरीमाथि तीन बालकलाई बलात्कार गर्न लगाएपछि</h6>
-              </a>
-              <a class="mini-newscard">
-                <figure>
-                  <img src="http://bg.annapurnapost.com/uploads/media/balatkar-case-morang_20200828030807.jpg" alt="">
-                </figure>
-                <h6>किशोरीमाथि तीन बालकलाई बलात्कार गर्न लगाएपछि</h6>
-              </a>
-            </li>
-
-          </ul>
-        </div>
-        <div class="advertisement ">
-          <figure>
-            <a href="http://subisu.net.np" title="Subusu clear TV" target="_blank">
-              <img
-                src="http://bg.annapurnapost.com/uploads/media/Annapurna-330x90_-250-Digital-ko-sath-ma---Copy_20180909143330.gif"
-                alt="pnpkhabar">
-            </a>
-          </figure>
-        </div>
-
-
-        <div class="advertisement ">
-          <figure><a href="http://annapurnapost.com/" title="College  of  Engineering" target="_blank">
-              <img src="http://bg.annapurnapost.com/uploads/media/College-Untitled-1_20180906113825.gif" alt="">
-            </a>
-          </figure>
+          <CategorySideAds />
         </div>
       </div>
     </div>
-
-
   </div>
-</div>
-
 </template>
 
 <script>
-  export default {
-    name: "category-page"
-  }
-
+export default {
+  name: "category-page",
+};
 </script>
 
 <style scoped lang="scss">
-
-
+.advertisement {
+  margin-bottom: 0.5em;
+}
 </style>
